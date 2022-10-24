bac8e4e49ba8c99e8e70fd38418e19ba6a29ee7b
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-10-24 14:30:58 +0300

    model relations

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,4 +1,27 @@
 import express from "express";
+import { verifyAdmin } from "../utils/verifyToken.js";
 const router = express.Router();
+import {
+  createRoom,
+  getAllRooms,
+  getRoom,
+  updateRoom,
+  deleteRoom,
+} from "../controllers/room.js";
+
+///CREATE ROOM
+router.post("/:hotelid", verifyAdmin, createRoom);
+
+///UPDATE ROOM
+router.put("/:id", verifyAdmin, updateRoom);
+
+///DELETE ROOM
+router.delete("/:id/:hotelid", verifyAdmin, deleteRoom);
+
+///GET ROOM
+router.get("/:id", getRoom);
+
+///GET ALL ROOMS
+router.get("/", getAllRooms);
 
 export default router;
